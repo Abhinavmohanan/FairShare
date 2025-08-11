9aa9bdbbf83cafdfd1dfe2b455b36a77693005cc
abhinav.mm <abhinav.mm@redbus.com>
2025-08-11 15:03:34 +0000

    added credits

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,13 +26,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
         <Navigation />
-        <main className="min-h-screen">
+        <main className="flex-1">
           {children}
         </main>
-        <footer className="bg-gray-50 border-t border-gray-200 py-6 mt-12">
+        <footer className="bg-gray-50 border-t border-gray-200 py-4 mt-auto">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="text-center">
               <p className="text-sm text-gray-600">
